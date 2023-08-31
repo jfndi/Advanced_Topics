aa08e0ab426b4dbe24fa04ff02440491c82d7147
jfndi <143571177+jfndi@users.noreply.github.com>
2023-08-31 15:04:18 +0200

    Added sleep project.

---
+++ sleep/sleep.cpp
@@ -0,0 +1,33 @@
+#include <format>
+#include <chrono>
+#include <thread>
+#include <iostream>
+
+using namespace std;
+
+using chrono::steady_clock;
+using chrono::duration;
+using this_thread::sleep_for;
+using this_thread::sleep_until;
+
+using namespace chrono_literals;
+
+constexpr void print(const string_view str_fmt, auto&&... args)
+{
+	fputs(vformat(str_fmt, make_format_args(args...)).c_str(), stdout);
+}
+
+int main()
+{
+	print("Let's wait a bit...\n");
+
+	auto t1 = steady_clock::now();
+	print("Sleep for 2.5 seconds.\n");
+	sleep_for(2s + 500ms);
+
+	print("Sleep for 3 seconds.\n");
+	sleep_until(steady_clock::now() + 3s);
+
+	duration<double> dur1 = steady_clock::now() - t1;
+	print("Total duration: {:.5}\n", dur1);
+}
