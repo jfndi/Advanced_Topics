f0696b4a260f2c637d9fc742de3d0deaf82a90d0
jfndi <143571177+jfndi@users.noreply.github.com>
2023-10-13 12:11:36 +0200

    Add span project.

---
+++ span/span.cpp
@@ -0,0 +1,26 @@
+#include <format>
+#include <span>
+
+using namespace std;
+
+constexpr void print(const string_view& str_fmt, auto&&... args)
+{
+	fputs(vformat(str_fmt, make_format_args(args...)).c_str(), stdout);
+}
+
+template <typename T>
+void pspan(const span<T> s)
+{
+	print("Number of elements: {}\n", s.size());
+	print("Size of span: {}\n", s.size_bytes());
+
+	for (auto e : s)
+		print("{} ", e);
+	print("\n");
+}
+
+int main()
+{
+	int ca1[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	pspan<int>(ca1);
+}
